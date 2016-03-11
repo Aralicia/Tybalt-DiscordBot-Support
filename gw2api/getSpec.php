5aee2bb561bd7c5ce936ecad9f86a6322b643111
GleipnirYmir <ymirgleipnir@gmail.com>
2016-03-11 12:27:04 -0300

    Working on !spec : basic setup

--- gw2api/getSpec.php
+++ gw2api/getSpec.php
@@ -1,1 +1,76 @@
+<?php 
 
+require_once(__DIR__.'/inc/common.inc.php');
+require_once(__DIR__.'/inc/database.inc.php');
+require_once(__DIR__.'/inc/api.inc.php');
+require_once(__DIR__.'/inc/gw2_facts.inc.php');
+require_once(__DIR__.'/inc/gw2_chatlink.inc.php');
+
+$args = parseArgs();
+$options = $args['options'];
+$pattern = $args['content'];
+$name = "";
+$raw = "";
+
+if (empty($pattern)) reply('I\'m sorry ?');
+$id = '';
+
+//$spec = [];
+//$spec = implode(' ', $pattern);
+
+$spec = $args['splits'];
+
+//Have to check this
+if (isId($pattern)) {
+    $id = $pattern;
+/*} else if (ChatLink::check($pattern)){
+    $chatlink = new ChatLink($pattern);
+    if ($chatlink->getHeader() != 'Specialization') reply('This is not a Specialization !');
+    $id = $chatlink->getId();*/
+} else {
+    $failure = null;
+    $id = getIdByName($pattern, 'specialization', $failure);
+    if ($id == FALSE) {
+        if (is_array($failure)) {
+            reply("I've found the following specializations : `".implode('`, `', $failure)."`. Which one do you want ?");
+        } else if ($failure < 1) {
+            reply("I've not found anything, sorry.");
+        } else {
+            reply("I've found ".$failure." matches. Can you be more precise ?");
+        }
+    }
+}
+/*
+function getSpecTraits($specData) {
+    $traitData = api()->v2('traits', ['params' => ['ids' => array_merge($specData->minor_traits, $specData->major_traits)]]);
+    $traits = (object)[
+        'adept' => (object)['minor' => null, 'major' => []],
+        'master' => (object)['minor' => null, 'major' => []],
+        'grandmaster' => (object)['minor' => null, 'major' => []]
+    ];
+    $tiers = [$traits->adept, $traits->master, $traits->grandmaster];
+
+    foreach($traitData as $trait) {
+        $tier = $tiers[$trait->tier];
+        if ($trait->slot == 'Minor') {
+            $tiers[$trait->tier]->minor = $trait;
+        } else {
+            $tiers[$trait->tier]->major[$trait->order] = $trait;
+        }
+    }
+    return $traits
+}
+
+$specData = api()->v2('specializations', ['params' => ['id' => $id]]);
+$traits = getSpecTraits($specData);
+
+$mytrait = $traits->master->minor;
+$mytrait = $traits->adept->major[2];
+*/
+
+$data = api()->v2('specializations', ['params' => ['id' => $id]]);
+if ($data == null) {
+    reply("I dont know this specialization, sorry.");
+}
+
+include(__DIR__.'/templates/spec.php');
---
+++ gw2api/templates/spec.php
@@ -0,0 +1,24 @@
+<?php
+require_once(dirname(__DIR__).'/inc/gw2_facts.inc.php');
+require_once(dirname(__DIR__).'/inc/gw2_chatlink.inc.php');
+
+function getTier($id) {
+    return ($id == 1 ? "Adept" : ($id == 2 ? "Master" : ($id == 3 ? "Grandmaster" : "")));
+}
+function getTraitTitle($trait, $verbose) {
+    return $trait->name.' • '.getNameById($trait->specialization, 'specialization').' • '.$trait->slot.' '.getTier($trait->tier).' trait';
+}
+function getSpecializationTitle($spec, $verbose) {
+//    return $spec->name.' • '.'proffesion'.' • '.' specialization';
+return 'testing';
+}
+$verbose = (in_array('v', $options) || in_array('verbose', $options));
+$debug = (in_array('d', $options) || in_array('debug', $options));
+if ($debug) {
+    reply(print_r($data));
+}
+$lines = [];
+//$lines[] = getTraitTitle($data, $verbose);
+$lines[] = getSpecializationTitle($spec, $verbose);
+//$lines[] = $data->description;
+reply(implode("\r\n", array_filter($lines)), true);
