56fcf9b2f74628731b44601fff5ba51d82ce8a9a
GleipnirYmir <ymirgleipnir@gmail.com>
2016-03-11 13:18:14 -0300

    Working on !spec : bugfixes

--- gw2api/templates/spec.php
+++ gw2api/templates/spec.php
@@ -8,10 +8,43 @@ function getTier($id) {
 function getTraitTitle($trait, $verbose) {
     return $trait->name.' • '.getNameById($trait->specialization, 'specialization').' • '.$trait->slot.' '.getTier($trait->tier).' trait';
 }
-function getSpecializationTitle($spec, $verbose) {
-//    return $spec->name.' • '.'proffesion'.' • '.' specialization';
-return 'testing';
+function getSpecType($specType){
+    return ($specType == 1 ? "Elite" : "Core");
 }
+function getSpecializationTitle($spec, $verbose) {
+    /*
+    $desc = 'Core specialization';
+    if($spec->elite){
+        $desc = 'Elite specialization';
+    }
+    */
+    return $spec->name.' • '.$spec->profession.' • '.getSpecType($spec->elite).' specialization';
+}
+function getSpecTraits($specData) {
+    $traitData = api()->v2('traits', ['params' => ['ids' => array_merge($specData->minor_traits, $specData->major_traits)]]);
+    $traits = (object)[
+        'adept' => (object)['minor' => null, 'major' => []],
+        'master' => (object)['minor' => null, 'major' => []],
+        'grandmaster' => (object)['minor' => null, 'major' => []]
+    ];
+    $tiers = [$traits->adept, $traits->master, $traits->grandmaster];
+    foreach($traitData as $trait) {
+        $tier = $tiers[$trait->tier];
+        if ($trait->slot == 'Minor') {
+            $tiers[$trait->tier]->minor = $trait;
+        } else {
+            $tiers[$trait->tier]->major[$trait->order] = $trait;
+        }
+    }
+    return $traits;
+}
+
+
+
+
+
+$traits = getSpecTraits($data);
+
 $verbose = (in_array('v', $options) || in_array('verbose', $options));
 $debug = (in_array('d', $options) || in_array('debug', $options));
 if ($debug) {
@@ -19,6 +52,6 @@ if ($debug) {
 }
 $lines = [];
 //$lines[] = getTraitTitle($data, $verbose);
-$lines[] = getSpecializationTitle($spec, $verbose);
-//$lines[] = $data->description;
+$lines[] = getSpecializationTitle($data, $verbose);
+$lines[] = $traits;
 reply(implode("\r\n", array_filter($lines)), true);
